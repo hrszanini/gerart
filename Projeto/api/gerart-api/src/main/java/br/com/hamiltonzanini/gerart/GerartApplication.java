38db9d1675915b1f35a1b93a2c22522c7cda8d14
Hamilton Zanini <hamilton.silva@fotosensores.com>
2022-09-10 04:12:56 -0300

    Atualização de projeto

---
+++ Projeto/api/gerart-api/src/main/java/br/com/hamiltonzanini/gerart/GerartApplication.java
@@ -0,0 +1,13 @@
+package br.com.hamiltonzanini.gerart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerartApplication.class, args);
+	}
+
+}
